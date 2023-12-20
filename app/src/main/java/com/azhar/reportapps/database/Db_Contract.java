19dcd63a9b912f9fdc6375dea837a9e730015e7d
chandra.wahyudi <chandrawahjoedi01@gmail.com>
2023-12-20 15:50:21 +0700

    Initial commit

---
+++ app/src/main/java/com/azhar/reportapps/database/Db_Contract.java
@@ -0,0 +1,10 @@
+package com.azhar.reportapps.database;
+
+public class Db_Contract {
+
+    public static String ip = "103.214.112.89";
+
+    public static final String urlRegister = "http://"+ip+"/my_api_android//api-register.php";
+    public static final String urlLogin = "http://"+ip+"/my_api_android//api-login.php";
+
+}
